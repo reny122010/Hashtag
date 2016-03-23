6ac4e853f9b79d4fd2f614a9f5ba9cf1bbe43303
Renê Alves <renealves@lavid.ufpb.br>
2016-03-23 01:13:28 -0300

    inserted the info at console and cases deal

--- routes/hashModule/v1/module.js
+++ routes/hashModule/v1/module.js
@@ -5,7 +5,12 @@ exports.module = function(dbConnection) {
 	router.put('/Hashtag/',function (reg, res){
 		console.log(res.body);
 		dbConnection.Hashtag.insert(reg.body,function(err,docs){
-			res.json(docs);
+			if(err != null){
+				res.json(docs);
+				console.log ("INFO: Successfully inserted!");
+			}else{
+				console.log ("INFO: Fail, not inserted!");
+			}
 		});
 	});
 	//Using restful, responsible for to remove element of Mongodb
@@ -13,13 +18,23 @@ exports.module = function(dbConnection) {
 		var id = req.params.id;
 		console.log(id);
 		db.Hashtag.remove({_id: mongojs.ObjectId(id)}, function (err, docs) {
-			res.json(docs);
+			if(err != null){
+				res.json(docs);
+				console.log ("INFO: Element removed!");
+			}else{
+				console.log ("INFO: Element not removed!");
+			}
 		});
 	});
 	//Using restful, responsible for to list element of Mongodb
 	router.get('/Hashtag/', function (req, res) {
-		dbConnection.Hashtag.find(function (err, doc) {
-			res.json(doc);
+		dbConnection.Hashtag.find(function (err, docs) {
+			if(err != null){
+				res.json(docs);
+				console.log ("INFO: Successfully listed!");
+			}else{
+				console.log ("INFO: Fail, not listed!");
+			}
 		});
 	});
 
